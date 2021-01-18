47aceda766d8c2aef1d6fcf4799ecace449740fb
glowstick-lamp <lampshade451@gmail.com>
2021-01-18 13:23:17 +0800

    added embed splitting into :roles

--- src/commands/member/roles.ts
+++ src/commands/member/roles.ts
@@ -28,22 +28,43 @@ class ListCatsCommand extends Command
 
     let embedSends:Promise<Message>[] = [];
 
-    results.forEach(async cat =>
+    for (const cat of results)
+    {
+      let roleString = '';
+      let counter = 0;
+      
+      for (let i = 0; i < cat.roles.length; ++i)
       {
-        let roleString = '';
-        cat.roles.forEach(r =>
-          {
-            roleString = `${roleString}${r.name} --- ${r.id}\n`
-          });
-        roleString = roleString.slice(0, roleString.length - 2);
-        embedSends.push(msg.channel.send({
-          embed: {
-            title: cat.name,
-            color: cat.defaultRoleColor,
-            description: roleString,
-          }
-        }));
-      });
+        const role = cat.roles[i];
+        roleString = `${roleString}${role.name} --- ${role.id}\n`
+        ++counter;
+        if (counter === 50 || i === cat.roles.length - 1)
+        {
+          embedSends.push(msg.channel.send({
+            embed: {
+              title: cat.name,
+              color: cat.defaultRoleColor,
+              description: roleString,
+              fields: [{
+                name: "id",
+                value: `\`${cat.id}\``,
+                inline: true,
+              }, {
+                name: "defaultRoleColor",
+                value: `\`${cat.defaultRoleColor}\``,
+                inline: true,
+              }, {
+                name: "Self-Assignable",
+                value: cat.selfAssignable ? "Yes" : "No",
+                inline: true,
+              }],
+            }
+          }));
+          counter = 0;
+          roleString = '';
+        }
+      }
+    }
     return await Promise.all(embedSends);
   }
 }
