89f3add9237d3486abb7509024e49fb024f22a2f
glowstick-lamp <lampshade451@gmail.com>
2021-01-19 19:15:55 +0800

    Adds fake fuzzy search for roles

--- src/commands/member/roles.ts
+++ src/commands/member/roles.ts
@@ -2,6 +2,7 @@ import { Command, CommandoClient, CommandoMessage } from "discord.js-commando"
 import { Category } from "../../entity/Category"
 import { getRepository, PrimaryColumn } from "typeorm"
 import { Message } from "discord.js"
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants"
 
 type RolesArgs = {
   catName: string;
@@ -29,7 +30,6 @@ class RolesCommand extends Command
   async run(msg: CommandoMessage, { catName }: RolesArgs)
   {
     let results = await getRepository(Category).find({
-      name: catName,
       selfAssignable: true,
       guild: {
         id: msg.guild.id,
@@ -39,28 +39,45 @@ class RolesCommand extends Command
     if (results.length === 0)
       return await msg.say(`${catName} category not found`);
 
-    let embedSends:Promise<Message>[] = [];
-
+    let foundCats: Map<number, Category> = new Map<number, Category>();
     for (const cat of results)
     {
+      let dbName = cat.name.toLowerCase();
+      let input = catName.toLowerCase().trim();
+      
+      let foundIndex = dbName.indexOf(input);
+      if (foundIndex !== -1)
+      {
+        if (!foundCats.has(foundIndex))
+          foundCats.set(foundIndex, cat);
+      }
+    }
+
+    if (foundCats.size > 0)
+    {
+      let i = 0;
+      while (!foundCats.has(i))
+        ++i;
+      let catAskedFor = foundCats.get(i);
       let roleString = '';
       let counter = 0;
+      let embedSends:Promise<Message>[] = [];
       
-      for (let i = 0; i < cat.roles.length; ++i)
+      for (let i = 0; i < catAskedFor.roles.length; ++i)
       {
-        const role = cat.roles[i];
-        roleString = `${roleString}${role.name}\n`
+        const role = catAskedFor.roles[i];
+        roleString = `${roleString}${role.name}, `
         ++counter;
-        if (counter === 50 || i === cat.roles.length - 1)
+        if (counter === 50 || i === catAskedFor.roles.length - 1)
         {
           embedSends.push(msg.channel.send({
             embed: {
-              title: cat.name,
-              color: cat.defaultRoleColor,
-              description: roleString,
+              title: catAskedFor.name,
+              color: catAskedFor.defaultRoleColor,
+              description: roleString.slice(0, roleString.length - 2),
               fields: [{
                 name: "defaultRoleColor",
-                value: `\`${cat.defaultRoleColor}\``,
+                value: `\`${catAskedFor.defaultRoleColor}\``,
                 inline: true,
               }],
             }
@@ -69,8 +86,8 @@ class RolesCommand extends Command
           roleString = '';
         }
       }
-    }
-    return await Promise.all(embedSends);
+      return await Promise.all(embedSends);
+    } return await msg.say(`${catName} category not found`);
   }
 }
 
