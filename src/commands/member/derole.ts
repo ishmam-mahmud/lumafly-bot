396036a4191095b7801dc058b93d82f454cb88a5
glowstick-lamp <lampshade451@gmail.com>
2021-01-19 19:15:20 +0800

    Adds fake fuzzy search and checks for role, derole

--- src/commands/member/derole.ts
+++ src/commands/member/derole.ts
@@ -1,5 +1,6 @@
 import { Command, CommandoClient, CommandoMessage } from "discord.js-commando"
 import { Category } from "../../entity/Category"
+import { Role } from "../../entity/Role"
 import { getRepository } from "typeorm"
 
 type DeroleCommandArgs = {
@@ -33,27 +34,64 @@ class DeroleCommand extends Command
 
   async run(msg: CommandoMessage, { roleName }: DeroleCommandArgs)
   {
+    let found = false;
+    for (const role of msg.member.roles.cache.values())
+    {
+      let dsRoleName = role.name.toLowerCase().trim();
+      let input = roleName.toLowerCase().trim();
+      if (dsRoleName.includes(input))
+      {
+        found = true;
+        break;
+      }
+    }
+    if (!found)
+      return await msg.say(`You don't have that role, whatever it is.`);
+
     let results = await getRepository(Category).find({
+      selfAssignable: true,
       guild: {
         id: msg.guild.id,
       }
     });
 
+    let foundRoles: Map<number, Role> = new Map<number, Role>();
     for (const cat of results)
     {
       for (const role of cat.roles)
       {
-        if (role.name === roleName)
+        let dbName = role.name.toLowerCase();
+        let input = roleName.toLowerCase().trim();
+
+        let foundIndex = dbName.indexOf(input);
+        if (foundIndex !== -1)
         {
-          if (cat.selfAssignable)
-          {
-            await msg.member.roles.remove(role.id);
-            return await msg.say(`access removed from role ${role.name}. congratulation ?`);
-          }
-          return await msg.say(`:pensive:`);
+          if (!foundRoles.has(foundIndex))
+            foundRoles.set(foundIndex, role);
         }
       }
     }
+
+    if (foundRoles.size > 0)
+    {
+      let i = 0;
+      while (!foundRoles.has(i))
+        ++i;
+      let roleToRemove = foundRoles.get(i); 
+      try
+      {
+        await msg.member.roles.remove(roleToRemove.id);
+      } catch (error)
+      {
+        let e = `msg.url\n${error.message}`;
+        console.error(msg.url);
+        console.error(error);
+        await msg.author.send(e);
+        return await msg.say(`:pensive: I failed`);  
+      }
+      return await msg.say(`access removed from role ${roleToRemove.name}. congratulation ?`);
+    }
+
     return await msg.say(`${roleName} not found !`);
   }
 }
--- src/commands/member/role.ts
+++ src/commands/member/role.ts
@@ -1,6 +1,7 @@
 import { Command, CommandoClient, CommandoMessage } from "discord.js-commando"
 import { Category } from "../../entity/Category"
 import { getRepository } from "typeorm"
+import { Role } from "../../entity/Role"
 
 type RoleCommandArgs = {
   roleName: string;
@@ -33,26 +34,58 @@ class RoleCommand extends Command
 
   async run(msg: CommandoMessage, { roleName }: RoleCommandArgs)
   {
+    for (const role of msg.member.roles.cache.values())
+    {
+      let dsRoleName = role.name.toLowerCase().trim();
+      let input = roleName.toLowerCase().trim();
+      if (dsRoleName.includes(input))
+      {
+        return await msg.say(`You already have the ${role.name} role`);
+      }  
+    }
+
     let results = await getRepository(Category).find({
+      selfAssignable: true,
       guild: {
         id: msg.guild.id,
       }
     });
 
+    let foundRoles: Map<number, Role> = new Map<number, Role>();
     for (const cat of results)
     {
       for (const role of cat.roles)
       {
-        if (role.name === roleName)
+        let dbName = role.name.toLowerCase();
+        let input = roleName.toLowerCase().trim();
+
+        let foundIndex = dbName.indexOf(input);
+        if (foundIndex !== -1)
         {
-          if (cat.selfAssignable)
-          {
-            await msg.member.roles.add(role.id);
-            return await msg.say(`access granted to role ${role.name}. congratulation !`);
-          }
-          return await msg.say(`access denied to role ${role.name}.`);
+          if (!foundRoles.has(foundIndex))
+            foundRoles.set(foundIndex, role);
         }
       }
+    }
+
+    if (foundRoles.size > 0)
+    {
+      let i = 0;
+      while (!foundRoles.has(i))
+        ++i;
+      let roleToAssign = foundRoles.get(i); 
+      try
+      {
+        await msg.member.roles.add(roleToAssign.id);
+      } catch (error)
+      {
+        let e = `msg.url\n${error.message}`;
+        console.error(msg.url);
+        console.error(error);
+        await msg.author.send(e);
+        return await msg.say(`:pensive: I failed`);  
+      }
+      return await msg.say(`access granted to role ${roleToAssign.name}. congratulation !`);
     }
 
     return await msg.say(`${roleName} not found !`);
