62886351f875b1a4d466af198a13c84bbfcc2926
glowstick-lamp <lampshade451@gmail.com>
2021-01-20 10:17:54 +0800

    removed prompts from role

--- src/commands/member/role.ts
+++ src/commands/member/role.ts
@@ -24,10 +24,7 @@ class RoleCommand extends Command
           key: "roleName",
           prompt: "What role do you want to add?",
           type: "string",
-          validate: (roleName: string) =>
-          {
-            return !/everyone/.exec(roleName) && roleName.length > 3;
-          },
+          default: "*"
         }
       ]
     })
@@ -35,6 +32,12 @@ class RoleCommand extends Command
 
   async run(msg: CommandoMessage, { roleName }: RoleCommandArgs)
   {
+    if (/everyone/.exec(roleName))
+      return await msg.say("no");
+    
+    if (roleName.length < 3)
+      return await msg.say("too few characters from the role name");
+
     for (const role of msg.member.roles.cache.values())
     {
       let dsRoleName = role.name.toLowerCase().trim();
@@ -65,13 +68,13 @@ class RoleCommand extends Command
     } catch (error)
     {
       console.error(error);
-      return await msg.say(`${roleName} role not found`);  
+      return await msg.say(`${roleName} role not found among self-assignable roles`);  
     }
 
     try
     {
       await msg.member.roles.add(foundRole.id);
-      return await msg.say(`access granted to role ${foundRole.name}. congratulation !`);
+      return await msg.say(`access granted to role ${foundRole.name}. congratulations !`);
     } catch (error)
     {
       await logErrorFromCommand(error, msg);
