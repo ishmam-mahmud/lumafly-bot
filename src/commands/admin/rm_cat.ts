5313437cb6253e986ea4e4d6ccae597d53add089
glowstick-lamp <lampshade451@gmail.com>
2021-01-21 14:36:48 +0800

    rm_cat uses logError

--- src/commands/admin/rm_cat.ts
+++ src/commands/admin/rm_cat.ts
@@ -2,6 +2,7 @@ import { Command, CommandoClient, CommandoMessage } from "discord.js-commando"
 import { Category } from "../../entity/Category"
 import { Role } from "../../entity/Role"
 import { getRepository } from "typeorm"
+import { logErrorFromCommand } from "../../utils";
 
 type RmCatCommandArgs = {
   nameCatToRemove: string,
@@ -32,44 +33,50 @@ class RmCatCommand extends Command
 
   async run(msg: CommandoMessage, { nameCatToRemove }: RmCatCommandArgs)
   {
-    if (/Uncategorized/.exec(nameCatToRemove))
-      return await msg.say("No removing the Uncategorized category");
-    
-    if (nameCatToRemove.length < 3)
-      return await msg.say("too few characters in the given cat's name");
-
-    let catToRemove = await getRepository(Category)
-      .findOne({
-        name: nameCatToRemove,
-        guild: {
-          id: msg.guild.id,
-        },
-      });
-
-    if (!catToRemove)
-      return await msg.say(`${nameCatToRemove} category does not exist!`);
-
-    let uncat = await getRepository(Category)
-      .findOne({
-        name: "Uncategorized",
-        guild: {
-          id: msg.guild.id,
-        }
-      });
-
-    let roles = catToRemove.roles.map(r =>
-      {
-        r.category = uncat;
-        return r;
-      })
-
-    uncat.roles = [...uncat.roles, ...roles];
-    catToRemove.roles = [];
-
-    await getRepository(Role).save(roles);
-    await getRepository(Category).save(uncat);
-    catToRemove = await getRepository(Category).remove(catToRemove);
-    return await msg.say(`${catToRemove.name} has been deleted`);
+    try
+    {
+      if (/Uncategorized/.exec(nameCatToRemove))
+        return await msg.say("No removing the Uncategorized category");
+      
+      if (nameCatToRemove.length < 3)
+        return await msg.say("too few characters in the given cat's name");
+  
+      let catToRemove = await getRepository(Category)
+        .findOne({
+          name: nameCatToRemove,
+          guild: {
+            id: msg.guild.id,
+          },
+        });
+  
+      if (!catToRemove)
+        return await msg.say(`${nameCatToRemove} category does not exist!`);
+  
+      let uncat = await getRepository(Category)
+        .findOne({
+          name: "Uncategorized",
+          guild: {
+            id: msg.guild.id,
+          }
+        });
+  
+      let roles = catToRemove.roles.map(r =>
+        {
+          r.category = uncat;
+          return r;
+        })
+  
+      uncat.roles = [...uncat.roles, ...roles];
+      catToRemove.roles = [];
+  
+      await getRepository(Role).save(roles);
+      await getRepository(Category).save(uncat);
+      catToRemove = await getRepository(Category).remove(catToRemove);
+      return await msg.say(`${catToRemove.name} has been deleted`);
+    } catch (error)
+    {
+      return await logErrorFromCommand(error, msg);
+    }
   }
 }
 
