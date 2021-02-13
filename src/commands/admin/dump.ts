67439b32dc17a96f03768a6f76664a554a262ceb
glowstick-lamp <lampshade451@gmail.com>
2021-02-13 15:10:00 +0800

    Dump uses query builder

--- src/commands/admin/dump.ts
+++ src/commands/admin/dump.ts
@@ -23,13 +23,34 @@ class DumpCommand extends Command
   {
     try
     {
-      let r = await getRepository(Role).find();
-      await getRepository(Role).remove(r);
-      let c = await getRepository(Category).find();
-      await getRepository(Category).remove(c);
-      let g = await getRepository(Guild).find();
-      await getRepository(Guild).remove(g);
-      return await msg.say(`${msg.guild.name} has been cleared. Run setup again`);
+      let roles = await getRepository(Role)
+        .createQueryBuilder("role")
+        .innerJoin("role.category", "cat")
+        .innerJoin("cat.guild", "guild")
+        .where("guild.id = :id", { id: msg.guild.id })
+        .select(["role.id"])
+        .getMany();
+
+      await getRepository(Role)
+        .createQueryBuilder("role")
+        .delete()
+        .whereInIds(roles.map(r => r.id))
+        .execute();
+
+      await getRepository(Category)
+        .createQueryBuilder("cat")
+        .innerJoin("cat.guild", "guild")
+        .delete()
+        .where("guild.id = :id", { id: msg.guild.id })
+        .execute();
+
+      await getRepository(Guild)
+        .createQueryBuilder("guild")
+        .delete()
+        .where("guild.id = :id", { id: msg.guild.id })
+        .execute();
+
+      return await msg.say(`${msg.guild.name} has been cleared. Run ${this.client.commandPrefix}setup again to reseed.`);
     } catch (error)
     {
       return await logErrorFromCommand(error, msg);
