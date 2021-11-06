9763dc73dba9799b720dc2f7085de655fa850d52
glowstick-lamp <lampshade451@gmail.com>
2021-11-06 17:38:08 +0800

    added roleEventHandlers

---
+++ src/core/events/roleUpdate.ts
@@ -0,0 +1,27 @@
+import dbClient from '../../db/client';
+import Event from './eventTypes';
+
+const RoleUpdateEvent: Event<'roleUpdate'> = {
+  name: 'roleUpdate',
+  once: false,
+  async execute(oldRole, newRole) {
+    if (oldRole.name === newRole.name) {
+      return;
+    }
+
+    const updatedRole = await dbClient.role.update({
+      where: {
+        id: newRole.id,
+      },
+      data: {
+        name: newRole.name,
+      },
+    });
+
+    console.log(
+      `Role with id ${updatedRole.id} updated name from ${oldRole.name} to ${updatedRole.name}`
+    );
+  },
+};
+
+export default RoleUpdateEvent;
