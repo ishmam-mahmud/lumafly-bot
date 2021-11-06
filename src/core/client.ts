086c8eefc3422911090cb7c8e43c75f0fc32465d
glowstick-lamp <lampshade451@gmail.com>
2021-11-06 20:05:23 +0800

    fix bug in roles add GUILD_MESSAGES intent

--- src/core/client.ts
+++ src/core/client.ts
@@ -2,7 +2,7 @@ import { Client, Intents } from 'discord.js';
 import getEnv from './getEnv';
 
 const discordClient = new Client({
-  intents: [Intents.FLAGS.GUILDS],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 discordClient.login(getEnv('CLIENT_TOKEN'));
