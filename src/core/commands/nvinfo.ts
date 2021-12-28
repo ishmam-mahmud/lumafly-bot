ab27c46ddf958a9a8bfa3cff1c480adc24105bda
glowstick-lamp <lampshade451@gmail.com>
2021-12-28 09:58:18 +0800

    removed unnecessary newlines in message

--- src/core/commands/nvinfo.ts
+++ src/core/commands/nvinfo.ts
@@ -7,16 +7,10 @@ const NvinfoCommand: Command = {
     const message = `**Non-/semi-verbal or non-/semi-scribal**
 
 **What is non-/semi-verbal or non-/semi-scribal?**
-Users who are self-described as such often have difficulty forming words and/or
-sentences for any number of reasons, whether that be anxiety, brain fog,
-exhaustion, etc. Most will use "non-/semi-verbal" but we have included the
-term "non-/semi-scribal" because discord is largely a text-based platform.
+Users who are self-described as such often have difficulty forming words and/or sentences for any number of reasons, whether that be anxiety, brain fog, exhaustion, etc. Most will use "non-/semi-verbal" but we have included the term "non-/semi-scribal" because discord is largely a text-based platform.
     
 **Why do some users type in emotes?**
-Some users may have more difficulty forming sentences than others, whether that
-is because they are non-/semi-verbal or non-/semi-scribal. Emotes lessen the
-strain and difficulty of forming sentences and allow these users to participate
-in chat.`;
+Some users may have more difficulty forming sentences than others, whether that is because they are non-/semi-verbal or non-/semi-scribal. Emotes lessen the strain and difficulty of forming sentences and allow these users to participate in chat.`;
     return await interaction.reply(message);
   },
 };
