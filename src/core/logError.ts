b3cac92689b0e32a1dd99b489dbcb3557a603b0f
Ishmam Mahmud <mdishmam99@outlook.com>
2022-02-01 14:56:48 +0800

    json stringify errors in sending them

--- src/core/logError.ts
+++ src/core/logError.ts
@@ -2,7 +2,9 @@ import discordClient from './client';
 import getEnv from './getEnv';
 
 export default async function logError(error: any) {
-  const logMessage = `Timestamp ${new Date().toISOString()}: ${error}`;
+  const logMessage = `Timestamp ${new Date().toISOString()}: ${JSON.stringify(
+    error
+  )}`;
   console.error(logMessage);
   console.error(error);
   const owner = await discordClient.users.fetch(getEnv('CLIENT_OWNER'));
